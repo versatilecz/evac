cc6fad5ffb9e49cfe08117e3fb2e96aa5700b3d7
Miksa <martin.miksanik@vacuumlabs.com>
2024-07-13 17:18:09 +0200

    BEtter build system

--- demo/build.rs
+++ demo/build.rs
@@ -2,8 +2,10 @@ fn main() {
     embuild::espidf::sysenv::relay();
     embuild::espidf::sysenv::output();
 
+    /*
     println!("cargo::rustc-cfg=esp_idf_eth_spi_ethernet_dm9051");
     println!("cargo::rustc-cfg=esp_idf_comp_esp_eth_enabled");
     println!("cargo::rustc-cfg=esp_idf_comp_esp_event_enabled");
     println!("cargo::rustc-cfg=esp_idf_eth_use_esp32_emac");
+    */
 }

e6cca9ba802ec638a12e9b60353e7fdfffe1646a
Jiri Peterek <jiri@peterek.net>
2025-10-20 10:09:31 +0200

    Remove unused module

--- frontend/packages/shared/src/websocket/helpers.ts
+++
@@ -1,6 +0,0 @@
-export function determineWebSocketURL(path: string, location: Location = self.location): URL {
-  const schema = location.protocol.replace('http', 'ws')
-  const hostname = location.host
-
-  return new URL(path, `${schema}//${hostname}`)
-}

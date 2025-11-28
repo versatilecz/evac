8f7b4892061e0316f50a2f9e53f70c6ba47c2770
Jiri Peterek <jiri@peterek.net>
2025-11-28 11:10:01 +0100

    Fix login screen

--- frontend/packages/auth/src/components/Wall.vue
+++ frontend/packages/auth/src/components/Wall.vue
@@ -8,7 +8,7 @@ const { isAuthenticated } = useAuth()
   <template v-if="isAuthenticated">
     <slot />
   </template>
-  <div class="grid grid-rows-[auto_1fr] w-screen h-screen bg-surface">
+  <div v-else class="grid grid-rows-[auto_1fr] w-screen h-screen bg-surface">
     <header class="h-20 flex px-6 items-center">
       <h1 class="text-2xl font-black">EVAC</h1>
     </header>
